1dd6151f87cf8522efb87c4371384f970229ba8b
EnriqueRodriguezDv <juanenriquerodriguezgarcia87@gmail.com>
2021-02-25 12:34:07 +0100

    eliminado user

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,46 +1,25 @@
 import React from "react";
 
+import styles from "./footer.module.scss";
+
 const Footer = () => {
   return (
     <>
-      <footer id="footer" className="footer bg-dark ">
-        <nav className="navbar navbar-expand justify-content-center navbar-dark "></nav>
-        <div className="row w-100 m-auto">
-          <div className="col-12 col-md-6 d-flex justify-content-center p-0 mt-4 mb-5">
-            {/* <a href="https://github.com/EnriqueRodriguezDv" target="_blank">
-            <img className="mr-4" src={github}></img>
+      <footer className={styles.footer}>
+        <section>
+          <h3>About Me</h3>
+          <a href="https://enriquerodriguezdv.github.io/porfolioReact/dist/index.html">
+            Web
           </a>
-          <a
-            href="https://www.facebook.com/Enrique-Rodriguez-Desarrollador-101686708213107"
-            target="_blank"
-          >
-            <img className="mr-4" src={facebook}></img>
-          </a>
-          <a
-            href="https://www.linkedin.com/in/desarrolladorenriquerodriguez/"
-            target="_blank"
-          >
-            <img src={linkedIn}></img>
-          </a> */}
-          </div>
-          <div className="col-12 col-md-6 d-flex align-items-center justify-content-center p-0">
-            <div
-              id="email"
-              className="d-flex align-items-center justify-content-center"
-              onClick={() => {
-                window.open("mailto:juanenriquerodriguezgarcia87@gmail.com");
-              }}
-            >
-              <h2 className="mr-4">Email</h2>
-              {/* <img src={gmail}></img> */}
-            </div>
-          </div>
-        </div>
-        <div className="row w-100 m-auto">
-          <div className="col-12 text-center mt-4 mb-1">
-            <small>Copyright 2020 by Kike Rodríguez</small>
-          </div>
-        </div>
+        </section>
+        <section>
+          <h3>Contact</h3>
+          <p>juanenriquerodriguezgarcia87@gmail.com</p>
+        </section>
+        <section>
+          <h3>Make for</h3>
+          <p>To practice next.js</p>
+        </section>
       </footer>
     </>
   );
--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Link from "next/link"
+import Link from "next/link";
 
-import styles from "./header.module.scss"
+import styles from "./header.module.scss";
 
 const Header = () => {
   return (
@@ -11,11 +11,8 @@ const Header = () => {
           <h2>Movies Fake</h2>
         </div>
         <nav className={styles.nav}>
-          <Link href={"/log-in"}>
-            <a>Log in</a>
-          </Link>
-          <Link  href={"/new-user"} >
-            <a className={styles.linkSeparator} >New User</a>
+          <Link href={"/create-movie"}>
+            <a className={styles.link}>Add movie</a>
           </Link>
         </nav>
       </header>
