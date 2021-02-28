f026503c6eee534ab2ada4c13782c7cf8cf2b5c2
EnriqueRodriguezDv <juanenriquerodriguezgarcia87@gmail.com>
2021-02-28 12:51:14 +0100

    pieceForm

--- components/PieceForm/PieceForm.js
+++ components/PieceForm/PieceForm.js
@@ -1,12 +1,12 @@
-import React from "react"
+import React from "react";
 
-const PieceForm = ({title, type}) => {
-    return (
-        <>
-            <p>{title}</p>
-            <input type={type}></input>
-        </>
-    )
-}
+const PieceForm = ({ title, type }) => {
+  return (
+    <>
+      <p>{title}</p>
+      <input type={type}></input>
+    </>
+  );
+};
 
-export default PieceForm
+export default PieceForm;
